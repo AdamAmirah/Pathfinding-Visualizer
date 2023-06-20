7f2ac428465950e879b7eff7b8aff34714bc1795
Amirah <adamamirah09@gmail.com>
2023-06-20 06:08:34 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,211 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { Box, Button, Flex, Stack } from "@chakra-ui/react";
+import * as React from "react";
+import { useState } from "react";
+import { start } from "repl";
+import { bfs } from "./algorithms/bfs";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+const numRows = 25;
+const numCols = 35;
+type ArrayElement = number;
+type TwoDimensionalArray = Array<Array<ArrayElement>>;
+
+export const App: React.FC = () => {
+  const [allowedToDraw, setAllowedToDraw] = useState<boolean>(true);
+  const [startButtonClicked, setStartButtonClicked] = useState<boolean>(false);
+  const [endButtonClicked, setEndButtonClicked] = useState<boolean>(false);
+  const [startPoint, setStartPoint] = useState<[number, number]>([-1, -1]);
+  const [endPoint, setEndPoint] = useState<[number, number]>([-1, -1]);
+  const [searching, setSearching] = React.useState<boolean>(false); // Track if the search is in progress
+
+  const twoDimensionalArray: TwoDimensionalArray = Array(numRows)
+    .fill(0)
+    .map(() => Array(numCols).fill(0));
+
+  const [grid, setGrid] =
+    React.useState<TwoDimensionalArray>(twoDimensionalArray);
+
+  const [mouseIsPressed, setMouseIsPressed] = React.useState(false);
+
+  const handleCellClick = (rowIndex: number, colIndex: number) => {
+    let newGrid = JSON.parse(JSON.stringify(grid));
+    if (allowedToDraw && grid[rowIndex][colIndex] <= 1)
+      newGrid[rowIndex][colIndex] = grid[rowIndex][colIndex] ? 0 : 1;
+    else if (startButtonClicked && grid[rowIndex][colIndex] === 0) {
+      if (startPoint[0] !== -1) {
+        newGrid[startPoint[0]][startPoint[1]] = 0; // start mode
+      }
+      newGrid[rowIndex][colIndex] = 2; // start mode
+      setStartPoint([rowIndex, colIndex]);
+    } else if (endButtonClicked && grid[rowIndex][colIndex] === 0) {
+      if (endPoint[0] !== -1) {
+        newGrid[endPoint[0]][endPoint[1]] = 0; // start mode
+      }
+      newGrid[rowIndex][colIndex] = 3; // end mode
+      setEndPoint([rowIndex, colIndex]);
+    }
+
+    setGrid(newGrid);
+  };
+
+  const handleCellMouseDown = (rowIndex: number, colIndex: number) => {
+    if (!allowedToDraw) return;
+    setMouseIsPressed(true);
+    handleCellClick(rowIndex, colIndex);
+  };
+
+  const handleCellMouseEnter = (rowIndex: number, colIndex: number) => {
+    if (!allowedToDraw) return;
+    if (mouseIsPressed) {
+      handleCellClick(rowIndex, colIndex);
+    }
+  };
+
+  const handleCellMouseUp = () => {
+    if (!allowedToDraw) return;
+    setMouseIsPressed(false);
+  };
+
+  React.useEffect(() => {
+    if (searching) {
+      const result = bfs(grid, startPoint, endPoint);
+      const path = result?.path;
+      const steps = result?.steps;
+
+      if (path) {
+        animateSearch(steps!, path);
+      } else {
+        console.log("No path found");
+      }
+      setSearching(false);
+    }
+  }, [searching, grid, startPoint, endPoint]);
+
+  const animateSearch = (
+    steps: Array<[number, number]>,
+    path: Array<[number, number]>
+  ) => {
+    const animationSpeed = 6;
+    let delay = 0;
+
+    for (let i = 0; i < steps.length; i++) {
+      const [row, col] = steps[i];
+
+      setTimeout(() => {
+        let newGrid = JSON.parse(JSON.stringify(grid));
+        newGrid[row][col] = 4; // Mark the cell as visited during search
+        setGrid(newGrid);
+      }, delay);
+
+      delay += animationSpeed;
+    }
+
+    setTimeout(() => {
+      let newGrid = JSON.parse(JSON.stringify(grid));
+      for (let i = 0; i < path.length; i++) {
+        const [row, col] = path[i];
+        newGrid[row][col] = 5; // Mark the cell as part of the path
+      }
+      setGrid(newGrid);
+    }, delay);
+  };
+
+  return (
+    <Flex>
+      <Stack direction="column">
+        <Button
+          bg="#F68E5F"
+          onClick={() => {
+            setAllowedToDraw(true);
+            setEndButtonClicked(false);
+            setStartButtonClicked(false);
+          }}
+        >
+          Draw lines
+        </Button>
+
+        <Button
+          bg="green"
+          onClick={() => {
+            setAllowedToDraw(false);
+            setEndButtonClicked(false);
+            setStartButtonClicked(true);
+          }}
+        >
+          Start point
+        </Button>
+
+        <Button
+          onClick={() => {
+            setAllowedToDraw(false);
+            setStartButtonClicked(false);
+            setEndButtonClicked(true);
+          }}
+          bg="red"
+        >
+          End point
+        </Button>
+        <Button
+          onClick={() => {
+            setSearching(true);
+          }}
+          bg="purple"
+        >
+          Start Search
+        </Button>
+        <Button
+          onClick={() => {
+            setGrid(twoDimensionalArray);
+            setStartPoint([-1, -1]);
+            setEndPoint([-1, -1]);
+          }}
+          bg="blue"
+        >
+          Clear
+        </Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          width: "fit-content",
+          margin: "0 auto",
+        }}
+      >
+        {grid.map((rows, rowIndex) =>
+          rows.map((col, colIndex) => (
+            <div
+              key={`${rowIndex}-${colIndex}`}
+              onMouseDown={() => handleCellMouseDown(rowIndex, colIndex)}
+              onMouseEnter={() => handleCellMouseEnter(rowIndex, colIndex)}
+              onMouseUp={handleCellMouseUp}
+              onClick={() => handleCellClick(rowIndex, colIndex)}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor:
+                  grid[rowIndex][colIndex] === 1
+                    ? "#F68E5F"
+                    : grid[rowIndex][colIndex] === 2
+                    ? "green"
+                    : grid[rowIndex][colIndex] === 3
+                    ? "red"
+                    : grid[rowIndex][colIndex] === 4
+                    ? "lightblue" // Color for visited cells during search
+                    : grid[rowIndex][colIndex] === 5
+                    ? "yellow" // Color for cells in the path
+                    : undefined,
+                border: "1px solid #595959",
+              }}
+            />
+          ))
+        )}
+      </div>
+    </Flex>
+  );
+};
+
+// [0, 0, 0]
+// [0, 0, 0]
+// [0, 0, 0]
+// [0, 0, 0]
+// [0, 0, 0]
---
+++ src/algorithms/bfs.ts
@@ -0,0 +1,83 @@
+type ArrayElement = number;
+type TwoDimensionalArray = Array<Array<ArrayElement>>;
+type ArrayPair = Array<[ArrayElement, ArrayElement]>;
+
+export const bfs = (
+  grid: TwoDimensionalArray,
+  start: [number, number],
+  target: [number, number]
+) => {
+  const numRows = grid.length;
+  const numCols = grid[0].length;
+
+  const directions: ArrayPair = [
+    [-1, 0], // Up
+    [0, 1], // Right
+    [1, 0], // Down
+    [0, -1], // Left
+    [-1, -1], // Diagonal: Up-Left
+    [-1, 1], // Diagonal: Up-Right
+    [1, 1], // Diagonal: Down-Right
+    [1, -1], // Diagonal: Down-Left
+  ];
+
+  const queue: ArrayPair = [start];
+  const visited = new Set<string>();
+  const parent: { [key: string]: [number, number] } = {};
+  const steps: Array<[number, number]> = []; // Track the steps taken during the search
+
+  while (queue.length > 0) {
+    const [row, col] = queue.shift()!;
+
+    if (row === target[0] && col === target[1]) {
+      return {
+        path: constructPath(parent, start, target),
+        steps: steps.slice(), // Return the steps taken during the search
+      };
+    }
+
+    for (const [dx, dy] of directions) {
+      const newRow: number = row + dx;
+      const newCol: number = col + dy;
+
+      if (
+        isValidCell(newRow, newCol, numRows, numCols) &&
+        !visited.has(`${newRow},${newCol}`) &&
+        grid[newRow][newCol] !== 1
+      ) {
+        queue.push([newRow, newCol]);
+        visited.add(`${newRow},${newCol}`);
+        parent[`${newRow},${newCol}`] = [row, col];
+        steps.push([newRow, newCol]); // Track the new step taken
+      }
+    }
+  }
+
+  return null;
+};
+
+const isValidCell = (
+  row: number,
+  col: number,
+  numRows: number,
+  numCols: number
+): boolean => {
+  return row >= 0 && row < numRows && col >= 0 && col < numCols;
+};
+
+const constructPath = (
+  parent: { [key: string]: [number, number] },
+  start: [number, number],
+  target: [number, number]
+): Array<[number, number]> => {
+  const path: Array<[number, number]> = [];
+  let current: [number, number] | undefined = target;
+
+  while (current && (current[0] !== start[0] || current[1] !== start[1])) {
+    path.unshift(current);
+    current = parent[`${current[0]},${current[1]}`];
+  }
+
+  path.unshift(start);
+  return path;
+};
