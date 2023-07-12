b453a34bf5c699752750ecea7a366b4fcd29a977
Amirah <adamamirah09@gmail.com>
2023-07-13 00:51:06 +0300

    Edit control panel

--- src/components/Main/control panel/ControlPanel.tsx
+++ src/components/Main/control panel/ControlPanel.tsx
@@ -14,31 +14,127 @@ const ControlPanel: React.FunctionComponent<IControlPanelProps> = (props) => {
   return (
     <Flex
       direction="column"
-      align="flex-start" // Align items to the left
       justify="center"
-      w="25%"
+      w="21%"
       // border="1px solid red"
+      my={20}
+      mx={20}
     >
+      <Heading mb={4} fontSize="2.5vh" fontFamily="prodsans">
+        Control Panel
+      </Heading>
       <LinkBox
         display="flex"
-        flexDirection="column"
         borderRadius="3xl"
         bg="#15171a"
         // border="1px solid red"
         w="100%"
-        alignItems="self-start"
-        pl={6}
+        alignItems="center"
+        px={8}
         py={4}
       >
-        <Flex align="center">
-          <Image src="play.svg" boxSize={10} mr={5} />
+        <Image src="play.svg" boxSize={8} />
+        <Flex ml={6} direction="column">
           <Heading size="md">
-            <LinkOverlay href="#">Visualize</LinkOverlay>
+            <LinkOverlay
+              color="#F2F2F2"
+              fontSize="lg"
+              fontFamily="prodsans"
+              href="#"
+            >
+              Visualize!
+            </LinkOverlay>
           </Heading>
+          <Text color="#73767C" fontSize="sm" fontFamily="prodsans">
+            State of the visualization
+          </Text>
         </Flex>
-        <Text color="whiteAlpha.400" mt={2}>
-          State of the visualization
-        </Text>
+      </LinkBox>
+      <LinkBox
+        display="flex"
+        borderRadius="3xl"
+        bg="#25272a"
+        // border="1px solid red"
+        w="95%"
+        alignItems="self-start"
+        px={8}
+        py={3}
+        my={2}
+        ml={4}
+      >
+        <Image src="clear_all-24px.svg" boxSize={12} />
+        <Flex ml={6} direction="column">
+          <Heading size="md">
+            <LinkOverlay
+              color="#F2F2F2"
+              fontSize="lg"
+              fontFamily="prodsans"
+              href="#"
+            >
+              Clear Board
+            </LinkOverlay>
+          </Heading>
+          <Text color="#73767C" fontSize="sm" fontFamily="prodsans">
+            Clear Board of Elements
+          </Text>
+        </Flex>
+      </LinkBox>
+      <LinkBox
+        display="flex"
+        borderRadius="3xl"
+        bg="#25272a"
+        // border="1px solid red"
+        w="95%"
+        alignItems="self-start"
+        px={8}
+        py={3}
+        my={2}
+        ml={4}
+      >
+        <Image src="Squiggly-Road-Sign-Arrow.svg" boxSize={12} />
+        <Flex ml={6} direction="column">
+          <Heading size="md">
+            <LinkOverlay
+              color="#F2F2F2"
+              fontSize="lg"
+              fontFamily="prodsans"
+              href="#"
+            >
+              Clear Path
+            </LinkOverlay>
+          </Heading>
+          <Text color="#73767C" fontSize="sm" fontFamily="prodsans">
+            Clear All Shortest-Path Nodes
+          </Text>
+        </Flex>
+      </LinkBox>
+      <LinkBox
+        display="flex"
+        borderRadius="3xl"
+        bg="#15171a"
+        // border="1px solid red"
+        w="100%"
+        alignItems="center"
+        px={8}
+        py={4}
+        my={2}
+      >
+        <Image src="dashboard.svg" boxSize={10} />
+        <Flex ml={6} direction="column">
+          <Heading size="md">
+            <LinkOverlay
+              color="#F2F2F2"
+              fontSize="lg"
+              fontFamily="prodsans"
+              href="#"
+            >
+              Speed!
+            </LinkOverlay>
+          </Heading>
+          <Text color="#73767C" fontSize="sm" fontFamily="prodsans">
+            Change Visualizer Speed
+          </Text>
+        </Flex>
       </LinkBox>
     </Flex>
   );
